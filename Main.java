f2057254db60fc4ddd98c96c448db8765c001b30
Margaret <144652524+margaret-ach@users.noreply.github.com>
2023-10-28 14:17:47 -0400

    added files for java

---
+++ Main.java
@@ -0,0 +1,16 @@
+package java_w3s;
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+         * Learning Java is really cool
+         * I hope to make software applications by the end
+         */
+
+        // Prints out Hello World
+        System.out.println("Hello World");
+
+        //Prints out 2
+        System.out.println(1 + 1);
+    }
+}
